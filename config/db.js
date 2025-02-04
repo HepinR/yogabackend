f46f94232c6cae616a1a0c3c462c269b2dcbeeba
HepinR <163547499+HepinR@users.noreply.github.com>
2025-02-04 13:25:03 +0530

    Update db.js

--- config/db.js
+++ config/db.js
@@ -12,6 +12,9 @@ const pool = mysql.createPool({
     waitForConnections: true,
     connectionLimit: 10,
     queueLimit: 0
+    ssl: process.env.NODE_ENV === 'production' ? {
+        rejectUnauthorized: true
+    } : false
 });
 
 // Convert pool to promise-based pool

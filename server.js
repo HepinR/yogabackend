b580ad0fbf932a79a145ed638fc1b59c6c9e7352
HepinR <163547499+HepinR@users.noreply.github.com>
2025-02-04 16:27:11 +0530

    Update server.js

--- server.js
+++ server.js
@@ -11,16 +11,7 @@ const allowedOrigins = [
     'https://yogafront.netlify.app',
     'http://localhost:3000'
 ];
-app.use(cors({
-    origin: function(origin, callback) {
-        if (!origin || allowedOrigins.indexOf(origin) !== -1) {
-            callback(null, true);
-        } else {
-            callback(new Error('Not allowed by CORS'));
-        }
-    },
-    credentials: true
-}));
+app.use(cors());
 app.use(express.json());
 
 // Root route
